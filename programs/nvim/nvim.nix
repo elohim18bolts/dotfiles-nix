c712db54009e92bb3133cae56afae1bbd26e6f13
Elohim18bolts <fduran@fduran.xyz>
2024-08-24 10:07:43 -0400

    ADD: initial lua config

--- programs/nvim/nvim.nix
+++ programs/nvim/nvim.nix
@@ -5,6 +5,21 @@ enable = true;
 viAlias = true;
 vimAlias = true;
 vimdiffAlias = true;
-
 };
+extraLuaConfig = ''
+vim.keymap.set("i", "jk", "<ESC>")
+vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.cursorline = true
+vim.opt.scrolloff = 15
+vim.opt.foldmethod = "expr"
+vim.opt.foldlevel = 20
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.binary = true
+vim.opt.endofline = false
+
+
+vim.cmd('autocmd BufRead,BufNewFile user-data set filetype=yaml')
+
+'';
 }
