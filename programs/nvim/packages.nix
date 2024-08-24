74f8aff0464e4a033af4f9910b3932b997264b5b
Elohim18bolts <fduran@fduran.xyz>
2024-08-24 10:41:09 -0400

    ADD: nvim pugins

---
+++ programs/nvim/packages.nix
@@ -0,0 +1,46 @@
+{pgks,...}:
+{
+  programs.neovim.plugins = with pkgs.vimPlugins; [
+    plenary-nvim
+    telescope-nvim
+    lualine-nvim
+    tabline-nvim
+    gruvbox-nvim
+    nvim-lspconfig
+    (nvim-treesitter.withPlugins (p: 
+    with p;[
+      org
+      nix
+      markdown
+      c
+      lua
+      rust
+      vimdoc
+      go
+      json5
+      yaml
+      javascript
+      typescript
+    ]
+    ))
+    harpoon
+    undotree
+    vim-fugitive
+    nvim-autopairs
+    nvim-cmp
+    cmp-buffer
+    cmp-path
+    cmp-cmdline
+    cmp-vsnip
+    vim-vsnip
+    ansible-vim
+    vim-nix
+    vim-floaterm
+    gitsigns-nvim
+    rainbow-delimiters-nvim
+    indent-blankline-nvim
+    hologram-nvim
+    go-nvim
+
+  ];
+}
