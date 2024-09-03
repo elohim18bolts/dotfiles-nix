823a851944feb8c317ba70fd047f9398a165acc3
elohim18bolts <fduran@fduran.xyz>
2024-09-03 15:43:53 -0400

    MOD: update flakes

--- flake.nix
+++ flake.nix
@@ -9,30 +9,30 @@
     };
   };
 
-  outputs = inputs@ { self, nixpkgs,home-manager,... }: 
-  let 
-    systems = nixpkgs.lib.systems.flakeExposed;
-    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f {pkgs = import nixpkgs { inherit system; };inherit system;});
-  in
-  {
-    defaultPackage = forAllSystems ({pkgs, system}: home-manager.defaultPackage."${system}");
-    packages = forAllSystems ({pkgs,system}: {
-      homeConfigurations ={
-        "darwin" = home-manager.lib.homeManagerConfiguration {
-                  # Note: I am sure this could be done better with flake-utils or something
-                  inherit pkgs;
-                  modules = [ ./systems/macos.nix ./home.nix ]; # Defined later
-              };
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      systems = nixpkgs.lib.systems.flakeExposed;
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f { pkgs = import nixpkgs { inherit system; };inherit system; });
+    in
+    {
+      defaultPackage = forAllSystems ({ pkgs, system }: home-manager.defaultPackage."${system}");
+      packages = forAllSystems ({ pkgs, system }: {
+        homeConfigurations = {
+          "darwin" = home-manager.lib.homeManagerConfiguration {
+            # Note: I am sure this could be done better with flake-utils or something
+            inherit pkgs;
+            modules = [ ./systems/macos.nix ./home.nix ]; # Defined later
+          };
 
-        "linux" = home-manager.lib.homeManagerConfiguration {
-                  # Note: I am sure this could be done better with flake-utils or something
-                  inherit pkgs;
-                  modules = [ ./systems/linux.nix ./home.nix ]; # Defined later
-              };
-            };
-    });
-    formatter = nixpkgs.lib.genAttrs systems (system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt);
+          "linux" = home-manager.lib.homeManagerConfiguration {
+            # Note: I am sure this could be done better with flake-utils or something
+            inherit pkgs;
+            modules = [ ./systems/linux.nix ./home.nix ]; # Defined later
+          };
+        };
+      });
+      formatter = nixpkgs.lib.genAttrs systems (system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt);
 
 
-  };
+    };
 }
