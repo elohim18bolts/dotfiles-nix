18acda21b65b342184f7217ad1be44814c1e474e
elohim18bolts <fduran@fduran.xyz>
2024-10-03 11:22:01 -0400

    MOD: new starship look

--- configs/starship.nix
+++ configs/starship.nix
@@ -16,18 +16,19 @@ let
   nord13 = "#ebcb8b";
   nord14 = "#a3be8c";
   nord15 = "#b48ead";
-  block1 = "[░▒▓](${nord4})$os$username$hostname[](bg:${nord3} fg:${nord4})";
+  block1 = "╭─[░▒▓](${nord4})$os$username$hostname[](bg:${nord3} fg:${nord4})";
   block2 = "$directory[](fg:${nord3} bg:${nord2})";
-  block3 = "$git_branch$git_status[](fg:${nord2} bg:${nord3})";
-  block4 = "$c$elixir$elm$golang$haskell$java$julia$nodejs$nim$rust$golang$php[](fg:${nord3} bg:${nord8})";
-  block5 = "$time$status[](fg:${nord8})\n";
-  block6 = "$character";
+  block3 = "$git_branch$git_status[](fg:${nord2})"; #bg:${nord3})";
+  block4 = "$c$elixir$elm$golang$haskell$java$julia$nodejs$nim$rust$golang$php "; #[](fg:${nord3} bg:${nord8})";
+  block5 = "$time$status"; #[](fg:${nord8})\n";
+  block6 = "╰─$character";
 in
 {
   programs.starship = {
     enable = true;
     settings = {
-      format = lib.concatStrings [ block1 block2 block3 block4 block5 block6 ];
+      format = lib.concatStrings [ block1 block2 block3 "\n" block6 ];
+      right_format = lib.concatStrings [ block4 block5 ];
       command_timeout = 1000;
       # You can also replace your username with a neat symbol like  to save some space
       username = {
@@ -50,7 +51,7 @@ in
         not_found_symbol = "";
         sigint_symbol = "";
         signal_symbol = "󱍖";
-        style = "fg:${nord11} bg:${nord8}";
+        style = "fg:${nord11}"; #bg:${nord8}";
 
 
       };
@@ -79,22 +80,22 @@ in
       nodejs = {
         symbol = "";
         style = "bg:${nord3}";
-        format = "[[ $symbol ($version) ](fg:${nord9} bg:${nord3})]($style)";
+        format = "[[ $symbol ($version) ](fg:${nord9})]"; #bg:${nord3})]($style)";
       };
       rust = {
         symbol = "";
         style = "bg:${nord3}";
-        format = "[[ $symbol ($version) ](fg:${nord9} bg:${nord3})]($style)";
+        format = "[[ $symbol ($version) ](fg:${nord9})]($style)"; #bg:${nord3})]($style)";
       };
       golang = {
         symbol = "";
         style = "bg:${nord3}";
-        format = "[[ $symbol ($version) ](fg:${nord9} bg:${nord3})]($style)";
+        format = "[[ $symbol ($version) ](fg:${nord9})]($style)"; # bg:${nord3})]($style)";
       };
       php = {
         symbol = " ";
         style = "bg:${nord3}";
-        format = "[[ $symbol ($version) ](fg:${nord9} bg:${nord3})]($style)";
+        format = "[[ $symbol ($version) ](fg:${nord9})]($style)"; # bg:${nord3})]($style)";
       };
       time = {
         disabled = true;
@@ -105,41 +106,41 @@ in
       c = {
         symbol = " ";
         style = "bg:${nord3}";
-        format = "[ $symbol ($version) ]($style)";
+        format = "[[ $symbol ($version) ](fg:${nord9})]($style)"; #bg:${nord3})]($style)";
       };
       docker_context = {
         symbol = " ";
-        style = "bg:${nord3}";
+        style = "fg:${nord9}";
         format = "[ $symbol $context ]($style) $path";
       };
       elixir = {
         symbol = " ";
-        style = "bg:${nord3}";
+        style = "fg:${nord9}";
         format = "[ $symbol ($version) ]($style)";
       };
       elm = {
         symbol = " ";
-        style = "bg:${nord3}";
+        style = "fg:${nord9}";
         format = "[ $symbol ($version) ]($style)";
       };
       haskell = {
         symbol = " ";
-        style = "bg:${nord3}";
+        style = "fg:${nord9}";
         format = "[ $symbol ($version) ]($style)";
       };
       java = {
         symbol = " ";
-        style = "bg:${nord3}";
+        style = "fg:${nord9}";
         format = "[ $symbol ($version) ]($style)";
       };
       julia = {
         symbol = " ";
-        style = "bg:${nord3}";
+        style = "fg:${nord9}";
         format = "[ $symbol ($version) ]($style)";
       };
       nim = {
         symbol = " ";
-        style = "bg:${nord3}";
+        style = "fg:${nord9}";
         format = "[ $symbol ($version) ]($style)";
       };
       os = {
