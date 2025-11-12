022d1d573f1e90a932852eed5c220db2d8befb4b
elohim18bolts <fduran@fduran.xyz>
2025-11-12 12:42:22 -0500

    UPDATE lf to use bat for text/* files

--- programs/lf/lf.nix
+++ programs/lf/lf.nix
@@ -18,12 +18,21 @@
             y=$5
             MIME=$(${pkgs.file}/bin/file -b --mime-type "$1")
             case "$MIME" in
+            text/*) ${pkgs.bat}/bin/bat --paging=never --color=always --line-range 1:30 "$1";;
+            image/*) ${pkgs.viu}/bin/viu -w "$2" -h "$3" $1; tput sc;;
             *) ${pkgs.pistol}/bin/pistol "$1";;
             esac
           '';
+        cleaner = pkgs.writeShellScriptBin "cleaner.sh" ''
+          tput rc
+          #tput ed
+
+        '';
+
       in
       ''
         set previewer  ${previewer}/bin/pv.sh
+        set cleaner ${cleaner}/bin/cleaner.sh
         map . set hidden!
       '';
   };
