bc51d138f31a972c892d473520956308d140d9cd
elohim18bolts <fduran@fduran.xyz>
2026-03-11 02:27:08 -0400

    REFACTOR: flake.nix

--- flake.nix
+++ flake.nix
@@ -9,39 +9,29 @@
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
     let
       systems = nixpkgs.lib.systems.flakeExposed;
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f { pkgs = import nixpkgs { inherit system; };inherit system; });
+      lib = nixpkgs.lib;
     in
     {
-      defaultPackage = forAllSystems ({ pkgs, system }: home-manager.defaultPackage."${system}");
       packages = forAllSystems
         ({ pkgs, system }: {
           homeConfigurations = {
-            "darwin" = home-manager.lib.homeManagerConfiguration {
+            "elohim" = home-manager.lib.homeManagerConfiguration {
               # Note: I am sure this could be done better with flake-utils or something
               inherit pkgs;
               extraSpecialArgs = {
                 inherit inputs;
-                system = "aarch64-darwin";
+                inherit system;
               };
-              modules = [ ./systems/macos.nix ./home.nix ]; # Defined later
-            };
-
-            "linux" = home-manager.lib.homeManagerConfiguration {
-              # Note: I am sure this could be done better with flake-utils or something
-              inherit pkgs;
-              extraSpecialArgs = {
-                inherit inputs;
-                system = "x86_64-linux";
-              };
-              modules = [ ./systems/linux.nix ./home.nix ]; # Defined later
+              modules = [ ./home.nix ]
+                ++ lib.optionals (pkgs.stdenv.isDarwin) [ ./systems/macos.nix ]
+                ++ lib.optionals (pkgs.stdenv.isLinux) [ ./systems/linux.nix ]; # Defined later
             };
           };
         });
       formatter = nixpkgs.lib.genAttrs systems (system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt);
-
-
     };
 }
