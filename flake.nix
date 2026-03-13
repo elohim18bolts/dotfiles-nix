37870e8c9f609a865f39eb73e0a3611131483043
elohim18bolts <fduran@fduran.xyz>
2026-03-13 13:17:21 -0400

    ADD: lima home for macos m1

--- flake.nix
+++ flake.nix
@@ -20,6 +20,7 @@
         default = self.homeConfigurations."elohim@mbp16";
         "elohim@mbp16" = mkHome "aarch64-darwin" [ ./systems/macos.nix ];
         "elohim@linux64" = mkHome "x86_64-linux" [ ./systems/linux.nix ];
+        "elohim@lima-aarch64" = mkHome "aarch64-linux" [ ./systems/lima.nix ];
 
       };
       formatter = nixpkgs.lib.genAttrs systems (system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt);
